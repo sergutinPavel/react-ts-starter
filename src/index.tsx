29471a145f1b0f7a75216746d8087900cfa1cd89
Pavel Sergutin <pavelsergutin@MacBook-Pro-Pavel.local>
2019-10-04 16:07:53 +0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
+import './styles/index.scss';
+
+import './polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('react-app-root') as HTMLElement,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/polyfills.ts
@@ -0,0 +1,2 @@
+import 'core-js';
+import 'core-js/es';
